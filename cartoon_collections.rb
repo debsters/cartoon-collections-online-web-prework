19f7472ff624e3a9d3631b99c8ad022041bfae2e
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-20 16:42:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,15 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select! do |x|
+    x.length > 4
+    end
+  if array.empty?
+    false
+  else
+    true
+  end
 end
 
 def find_the_cheese# code an argument here
